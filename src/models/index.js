3873e7d54be753e686abeb290c6746e7048b56b0
khushburajak <khushburajak17@gmail.com>
2024-02-25 13:23:47 +0545

    refactor

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+export { default as Category } from "./Category";
+export { default as Event } from "./Event";
+export { default as Profile } from "./Profile";
+export { default as User } from "./User";
+export { default as Story } from "./Story";
+
